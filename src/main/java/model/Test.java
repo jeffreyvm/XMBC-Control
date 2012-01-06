110081f07bc0bff2256907ad0b95914c67e5b607
Firstname Lastname <jeffreyvanmulem@gmail.com>
2012-01-06 18:55:04 +0100

    Initial Commit

---
+++ src/main/java/model/Test.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jeffrey
+ * Date: 06/01/12
+ * Time: 18:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
